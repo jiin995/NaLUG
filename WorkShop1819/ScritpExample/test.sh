ab3a68d83cac0ac1fcb9232ebc9509e5874b84cf
Gabriele Previtera <gabrieleprevitera@gmail.com>
2019-04-05 23:54:47 +0200

    Pulizia e Rinominata cartella

--- WorkShop1819/ScritpExample/test.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
- if [ $# -eq 1 ]; then
-	 INTERFACE=$1
-	 IP=$(sudo ifconfig $INTERFACE | grep "inet" | awk '{print $2'})
-	 echo "L'ip di $INTERFACE è $IP"
-fi
-x=(3 2)
-echo ${x[0]}
-
