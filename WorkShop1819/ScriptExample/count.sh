f748dd88b13b68200140941aa1f9b265873fd490
jiin995 <gabrieleprevitera@gmail.com>
2019-04-07 17:55:57 +0200

    Aggiornato

--- WorkShop1819/ScriptExample/count.sh
+++ WorkShop1819/ScriptExample/count.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
+#-----------------------------------------------------------------------------
+# NaLUG@UniNA
+# 
+# Conta il numero di occorrenze di una parola nei file .txt di una cartella
+#
+#-----------------------------------------------------------------------------
 
-#Verifico che ci sia almeno un parametro in input
-if [ $# -ne 1 ]; then
-	echo "Uso: ./count.sh perolaDaCercare"
+#Verifico che ci siano esattamente due parametri in input
+if [ $# -ne 2 ]; then
+	echo -e "Uso :\n\t./count.sh WORD PATH"
+	echo -e "Esempio :\n\t./count.sh 123456 DataSets"
 	exit
 fi
 
 #salvo il parametro dato in ingresso in una variabile
-TEXT=$1
+WORD=$1
+MYPATH=$2
 TOT_OCC=0
 
-#salvo la lista dei file,escluso count.sh, in un array
-FILES=( $(ls -l| grep -v "*.sh" | awk '{print $9}'))
+#salvo la lista dei file che in $PATH hanno estensione txt in un array
+FILES=( $(ls -l $MYPATH | grep "txt" | awk '{print $9}'))
 
 #per ogni file nella lista conto il numero di occorrenze della parola in input
-for i in $(seq 0 $((${#FILES[@]}-1))); do 
-	echo "Trovo il testo $TEXT in ${FILES[i]}"
-	OCCORRENZE=( $( grep  -w "$TEXT" ${FILES[i]} | wc -l ))i
+for i in $(seq 0 $((${#FILES[@]}-1))); do
+
+	echo "Trovo il testo $WORD in $MYPATH/${FILES[i]}"
+
+	#Cerco all'interno del file il testo e ne conto il numero di occorrenze
+	OCCORRENZE=( $( grep  -w "$WORD" $MYPATH/${FILES[i]} | wc -l ))
 	
-	#salvo il numero totale di occorrenze
+	#Aggiorno il numero totale di occorrenze
 	TOT_OCC=$(($TOT_OCC+$OCCORRENZE))
 
 	echo "Nel file ${FILES[i]} la parola $TEXT è stata trovata $OCCORRENZE volte"
